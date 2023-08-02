fb6c6c612683267926857b4020c1081df9f01296
udodinho <udodinho200@gmail.com>
2023-08-02 13:43:34 -0700

    Create login user service

--- src/service/user-service.ts
+++ src/service/user-service.ts
@@ -1,5 +1,6 @@
-import  User from "../models/user-model";
-import { ValidationError } from "../errors/index";
+import  User, { UserDocument } from "../models/user-model";
+import { omit } from "lodash";
+import { ValidationError, UnauthenticatedError } from "../errors/index";
 
 type payload = {
     email: string;
@@ -21,3 +22,45 @@ type payload = {
     
     return { user: { name: user.name }, token };
 };
+
+export const login = async (input: UserDocument) => {
+    const { email, password } = input;
+
+    if (!email || !password) {
+        throw new ValidationError("Please provide an email or password");
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new UnauthenticatedError("User not found");
+    }
+
+    const isPassword = await validPassword({ email, password });
+    
+    if (!isPassword) {
+        throw new ValidationError("Password is incorrect");
+    }
+
+    const token = user.createJWT();
+
+    return { user: {name: user.name }, token};
+}
+
+export const validPassword = async function ({ email, password, }: { email: UserDocument["email"], password: string }) {
+    
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new UnauthenticatedError("User not found");
+    }
+
+    const isMatch = await user.comparePassword(password);    
+
+    if (!isMatch) {
+        throw new ValidationError("Password is incorrect");
+    }
+
+    return omit(user.toJSON(), "password");
+}
+
